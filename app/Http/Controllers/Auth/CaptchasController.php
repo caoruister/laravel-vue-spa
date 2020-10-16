38ecc1b17d6ef726de2d2b6297efa7e8242679ed
曹锐 <caoruister@gmail.com>
2020-10-16 18:25:58 +0800

    add captcha

---
+++ app/Http/Controllers/Auth/CaptchasController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+use Mews\Captcha\Captcha;
+
+class CaptchasController extends Controller
+{
+    //
+    public function store(Request $request, Captcha $captchaBuilder)
+    {
+        $this->validate($request, [
+            'phone' => [
+                'required',
+                'regex:/^1(3\d|4[5-8]|5[0-35-9]|6[567]|7[01345-8]|8\d|9[025-9])\d{8}$/',
+                'unique:users',
+            ],
+        ], [], [
+            'phone' => '手机号码'
+        ]);
+
+        $key = 'captcha-'.Str::random(15);
+        $phone = $request->phone;
+
+        $captcha = $captchaBuilder->create('flat', true);
+        $expiredAt = now()->addMinutes(2);
+        Cache::put($key, ['phone' => $phone, 'code' => $captcha['key']], $expiredAt);
+
+        $result = [
+            'captcha_key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+            'captcha_image_content' => $captcha['img']
+        ];
+
+        return response()->json($result)->setStatusCode(201);
+    }
+}
