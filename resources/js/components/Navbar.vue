a430a72944006ae9060e1f43841f412d954ab5a3
曹锐 <caoruister@gmail.com>
2020-10-19 20:03:49 +0800

    change ui

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -12,7 +12,7 @@
           </router-link>
         </div>
       </div>
-      <div class="tablet-menu-wrapper-outer" v-bind:class="{ 'shown': !collapsed }">
+      <div class="tablet-menu-wrapper-outer" v-bind:class="{'shown':!collapsed}">
         <div class="tablet-menu-wrapper-inner">
           <div class="tablet-menu">
             <div class="row row-info">
@@ -21,28 +21,41 @@
               </div>
               <div class="col-xs-10">
                 <div class="name">{{user.name}}</div>
-                <div class="email">{{user.email}}</div>
+                <div class="email">{{user.phone}}</div>
               </div>
             </div>
-            <div class="link-item feedback ">
-              <a href=""><i class="icon-feedback"></i>Send Feedback</a>
+            <div class="link-item">
+              <router-link :to="{ name: 'settings.profile' }">
+                <i class="icon-header-icon5"></i>个人设置
+              </router-link>
             </div>
-            <div class="link-item intercom">
-              <a href=""><i class="icon-help"></i>Help</a>
+            <div class="link-item">
+              <a href=""><i class="icon-feedback"></i>反馈</a>
+            </div>
+            <div class="link-item">
+              <a href=""><i class="icon-help"></i>帮助</a>
             </div>
             <div class="link-item" @click.prevent="logout">
-              <a><i class="icon-header-icon6"></i>Logout</a>
+              <a><i class="icon-header-icon6"></i>登出</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="collapse navbar-collapse authenticated" v-bind:class="{'in' : !collapsed}" id="bs-example-navbar-collapse-1">
+      <div class="collapse navbar-collapse authenticated" id="bs-example-navbar-collapse-1">
         <div class="mobile-menu">
-          <div class="link-item intercom">
-            <a href="mailto:jg6t4w18@incoming.intercom.io" class="intercom"><i class="icon-help"></i>Help</a>
+          <div class="link-item">
+            <router-link :to="{ name: 'settings.profile' }">
+              <i class="icon-header-icon5"></i>个人设置
+            </router-link>
+          </div>
+          <div class="link-item">
+            <a class="feedback"><i class="icon-feedback"></i>反馈</a>
+          </div>
+          <div class="link-item">
+            <a class="intercom"><i class="icon-help"></i>帮助</a>
           </div>
           <div class="link-item" @click.prevent="logout">
-            <a><i class="icon-header-icon6"></i>Logout</a>
+            <a><i class="icon-header-icon6"></i>登出</a>
           </div>
         </div>
         <ul class="nav navbar-nav navbar-right">
@@ -53,7 +66,7 @@
             <ul class="dropdown-menu">
               <li class="user-info">
                 <div class="name">{{user.name}}</div>
-                <div class="email">{{user.email}}</div>
+                <div class="email">{{user.phone}}</div>
               </li>
               <li>
                 <router-link :to="{ name: 'settings.profile' }">
@@ -67,7 +80,7 @@
               <li><a class="logout" @click.prevent="logout"><i class="icon-header-icon6 normal"></i><i class="icon-header-icon6-hover hover"></i>登出</a></li>
             </ul>
           </li>
-          <li class="user-dropdown-business" ng-show="!isUserNameBusy">
+          <li class="user-dropdown-business" v-bind:class="{'opened':!collapsed}" v-on:click="collapsed = !collapsed">
             <i class="icon-header-icon3 toggle-icon open"></i>
             <i class="icon-header-icon2 toggle-icon close-icon"></i>
           </li>
@@ -108,10 +121,12 @@ export default {
 }
 </script>
 
-<style scoped>
-.profile-photo {
-  width: 2rem;
-  height: 2rem;
-  margin: -.375rem 0;
-}
+<style lang="scss" scoped>
+
+  @media (max-width: 767px) {
+    .navbar.navbar-default.navbar-main .navbar-collapse.collapse.in.authenticated .navbar-left, .navbar.navbar-default.navbar-main .navbar-collapse.collapse.show.authenticated .navbar-right {
+      display: none;
+    }
+  }
+
 </style>
