7bd3d43578efa1ed8c020d0978894a8a1da627ff
曹锐 <caoruister@gmail.com>
2020-11-03 11:46:24 +0800

    add alipay mobile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
-            $config['notify_url'] = route('payment.alipay.notify');//'http://requestbin.net/r/1fdygvk1';
+            $config['notify_url'] = 'https://uidpass.com/api/payment/alipay/notify';//route('payment.alipay.notify');//'http://requestbin.net/r/1fdygvk1';
             $config['return_url'] = config('app.url').'/payment/success';
 
             // 判断当前项目运行环境是否为线上环境
